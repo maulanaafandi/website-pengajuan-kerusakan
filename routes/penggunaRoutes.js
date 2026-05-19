94c43167e39b540644ba07062f66a4569ccce6ee
maulanaafandi <maulanaafandi0005@gmail.com>
2026-05-19 18:20:25 +0700

    merapikan

--- routes/penggunaRoutes.js
+++ routes/penggunaRoutes.js
@@ -24,57 +24,17 @@ router.post('/api/pengguna/laporan', authUser, (req, res, next) => {
   });
 }, PenggunaController.createLaporan);
 
-router.get(
-  '/api/pengguna/riwayat-laporan',
-  authUser,
-  PenggunaController.riwayatLaporan
-);
+router.get('/api/pengguna/riwayat-laporan',authUser,PenggunaController.riwayatLaporan);
+router.get('/api/pengguna/riwayat-laporan/:id',authUser,PenggunaController.detailLaporan);
+router.get('/api/pengguna/update-status',authUser,PenggunaController.updateStatusKaleb);
+router.get('/api/pengguna/update-status-kaleb',authUser,PenggunaController.updateStatusKaleb);
+router.get('/api/pengguna/update-laporan-kaleb',authUser,PenggunaController.updateStatusKaleb);
 
-router.get(
-  '/api/pengguna/riwayat-laporan/:id',
-  authUser,
-  PenggunaController.detailLaporan
-);
+router.get('/api/kaleb/update-status',authUser,PenggunaController.updateStatusKaleb);
 
-router.get(
-  '/api/pengguna/update-status',
-  authUser,
-  PenggunaController.updateStatusKaleb
-);
+router.get('/api/pengguna/kaleb/update-status',authUser,PenggunaController.updateStatusKaleb);
+router.put('/api/pengguna/kaleb/update-laporan',authUser,PenggunaController.updateLaporanKaleb);
 
-router.get(
-  '/api/pengguna/update-status-kaleb',
-  authUser,
-  PenggunaController.updateStatusKaleb
-);
-
-router.get(
-  '/api/pengguna/update-laporan-kaleb',
-  authUser,
-  PenggunaController.updateStatusKaleb
-);
-
-router.get(
-  '/api/kaleb/update-status',
-  authUser,
-  PenggunaController.updateStatusKaleb
-);
-
-router.get(
-  '/api/pengguna/kaleb/update-status',
-  authUser,
-  PenggunaController.updateStatusKaleb
-);
-router.put(
-  '/api/pengguna/kaleb/update-laporan',
-  authUser,
-  PenggunaController.updateLaporanKaleb
-);
-
-router.put(
-  '/api/pengguna/change-password',
-  authUser,
-  PenggunaController.changePassword
-);
+router.put('/api/pengguna/change-password',authUser,PenggunaController.changePassword);
 
 module.exports = router;
