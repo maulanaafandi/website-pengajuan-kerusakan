0046b2c9ae1a7b79733d2c93662223293a75fc45
maulanaafandi <maulanaafandi0005@gmail.com>
2026-05-21 18:54:27 +0700

    remove

--- add_dosen_kaleb_to_ruangan.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE ruangan
-  ADD COLUMN id_user INT NULL AFTER lokasi;
-
--- Jika kolom dosen_kaleb sudah pernah dibuat dan berisi id_user, pindahkan datanya:
--- ALTER TABLE ruangan ADD COLUMN id_user INT NULL AFTER lokasi;
--- UPDATE ruangan SET id_user = dosen_kaleb WHERE dosen_kaleb REGEXP '^[0-9]+$';
